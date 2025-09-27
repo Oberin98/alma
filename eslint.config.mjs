91258158c8720377ad73f8a690508fe425e3215d
Artem Beliaev <artembeliaev@haveignition.com>
2025-09-27 08:27:38 +0200

    Add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from "eslint/config";
+import eslintPluginPrettier from "eslint-config-prettier/flat";
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,17 +12,10 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default defineConfig([
+  eslintPluginPrettier,
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
   },
-];
-
-export default eslintConfig;
+]);
